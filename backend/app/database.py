a5676cc0f38e3479dc873f9cf3f91fb2e73e1ce9
Noel <noel.uran@gmail.com>
2025-12-22 12:03:10 +0100

    Added CRUD API endopoints for users, movies and ratings, testing done on /docs

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+from sqlalchemy.orm import Session
+from fastapi import Depends
+
 DATABASE_URL = "postgresql://movie_user:movie_pass@localhost:5432/movie_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from .database import engine
 from .models import Base
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from .database import get_db
+from .models import User
+from .models import Movie
+from .models import Rating
 
 app = FastAPI(title="Movie recommender API")
 
@@ -10,3 +16,38 @@ Base.metadata.create_all(bind=engine)
 
 def health_check():
     return {"status":"ok"}
+
+#Endpoint for users
+@app.post("/users")
+def create_user(email: str, db: Session = Depends(get_db)):
+    user = User(email=email)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+#Endpoint for movies
+@app.post("/movies")
+def create_movie(title: str, genre: str, year: int, db: Session = Depends(get_db)):
+    movie = Movie(title=title, genre=genre, year=year)
+    db.add(movie)
+    db.commit()
+    db.refresh(movie)
+    return movie
+
+
+@app.get("/movies")
+def get_movies(db: Session = Depends(get_db)):
+    return db.query(Movie).all()
+
+#Endpoint for ratings
+@app.post("/ratings")
+def add_rating(user_id: int, movie_id: int, rating: int, db: Session = Depends(get_db)):
+    rating_obj = Rating(
+        user_id=user_id,
+        movie_id=movie_id,
+        rating=rating
+    )
+    db.add(rating_obj)
+    db.commit()
+    return {"message": "Rating added"}
