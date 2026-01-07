8f0bb28ceead1bcbefa31c8218babb8ff03cae57
Noel <noel.uran@gmail.com>
2026-01-07 21:47:30 +0100

    Added MinIO object storage and export ratings dataset (csv) via batch processing

--- backend/batch/run_batch.py
+++ backend/batch/run_batch.py
@@ -5,6 +5,10 @@ from datetime import datetime
 from app.database import SessionLocal
 from app.models import Rating, MovieStats
 
+import csv
+from minio import Minio
+from io import StringIO, BytesIO
+
 
 def run_batch():
     db: Session = SessionLocal()
@@ -43,6 +47,40 @@ def run_batch():
     db.close()
 
     print("Batch obrada završena.")
+    export_ratings_dataset(db)
+
+def export_ratings_dataset(db: Session):
+    ratings = db.query(Rating).all()
+
+    csv_buffer = StringIO()
+    writer = csv.writer(csv_buffer)
+    writer.writerow(["user_id", "movie_id", "rating"])
+
+    for r in ratings:
+        writer.writerow([r.user_id, r.movie_id, r.rating])
+
+    data = csv_buffer.getvalue().encode("utf-8")
+    data_stream = BytesIO(data)
+
+    client = Minio(
+        "minio:9000",
+        access_key="minioadmin",
+        secret_key="minioadmin",
+        secure=False
+    )
+
+    bucket_name = "datasets"
+
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+
+    client.put_object(
+        bucket_name,
+        "ratings_dataset.csv",
+        data_stream,
+        length=len(data),
+        content_type="text/csv"
+    )
 
 
 if __name__ == "__main__":
